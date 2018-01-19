d91a64673e171c273616a59bdcf2971418ceb2de
JbLb <jerome@jblb.net>
2018-01-19 12:29:55 +0100

    new http server from FSBrowser examples

--- common/zmlserver.ino
+++ common/zmlserver.ino
@@ -519,8 +519,53 @@ void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t lenght
     }
 
 }
-void GetMessage() {
+
+//format bytes
+String formatBytes(size_t bytes){
+  if (bytes < 1024){
+    return String(bytes)+"B";
+  } else if(bytes < (1024 * 1024)){
+    return String(bytes/1024.0)+"KB";
+  } else if(bytes < (1024 * 1024 * 1024)){
+    return String(bytes/1024.0/1024.0)+"MB";
+  } else {
+    return String(bytes/1024.0/1024.0/1024.0)+"GB";
+  }
 }
+
+String getContentType(String filename){
+  if(http_server.hasArg("download")) return "application/octet-stream";
+  else if(filename.endsWith(".htm")) return "text/html";
+  else if(filename.endsWith(".html")) return "text/html";
+  else if(filename.endsWith(".css")) return "text/css";
+  else if(filename.endsWith(".js")) return "application/javascript";
+  else if(filename.endsWith(".png")) return "image/png";
+  else if(filename.endsWith(".gif")) return "image/gif";
+  else if(filename.endsWith(".jpg")) return "image/jpeg";
+  else if(filename.endsWith(".ico")) return "image/x-icon";
+  else if(filename.endsWith(".xml")) return "text/xml";
+  else if(filename.endsWith(".pdf")) return "application/x-pdf";
+  else if(filename.endsWith(".zip")) return "application/x-zip";
+  else if(filename.endsWith(".gz")) return "application/x-gzip";
+  return "text/plain";
+}
+
+bool handleFileRead(String path){
+  USE_SERIAL.println("handleFileRead: " + path);
+  if(path.endsWith("/")) path += "index.html";
+  String contentType = getContentType(path);
+  String pathWithGz = path + ".gz";
+  if(SPIFFS.exists(pathWithGz) || SPIFFS.exists(path)){
+    if(SPIFFS.exists(pathWithGz))
+      path += ".gz";
+    File file = SPIFFS.open(path, "r");
+    size_t sent = http_server.streamFile(file, contentType);
+    file.close();
+    return true;
+  }
+  return false;
+}
+
 
 void setup() {
     // USE_SERIAL.begin(921600);
@@ -543,6 +588,19 @@ void setup() {
         USE_SERIAL.flush();
         delay(1000);
     }
+
+// from examples/FSBrower/FSBrower.ino
+  SPIFFS.begin();
+  {
+    Dir dir = SPIFFS.openDir("/");
+    while (dir.next()) {
+      String fileName = dir.fileName();
+      size_t fileSize = dir.fileSize();
+      USE_SERIAL.printf("FS File: %s, size: %s\n", fileName.c_str(), formatBytes(fileSize).c_str());
+    }
+    USE_SERIAL.printf("\n");
+  }
+//**********************************
 
     // WiFi.disconnect();
     pixels.begin();
@@ -585,7 +643,14 @@ void setup() {
     initTint2rgb();
 
     http_server.begin();
-    http_server.on("/", GetMessage);
+  // from FSBrower.ino
+  //called when the url is not defined here
+  //use it to load content from SPIFFS
+  http_server.onNotFound([](){
+    if(!handleFileRead(http_server.uri()))
+      http_server.send(404, "text/plain", "FileNotFound");
+  });
+
 
     webSocket.begin();
     webSocket.onEvent(webSocketEvent);
@@ -593,6 +658,7 @@ void setup() {
 
 void loop() {
     webSocket.loop();
+    http_server.handleClient();
 
     if (gNextActionTime != -1 && gNextActionTime <= millis()) {
         gCurrentAction();
